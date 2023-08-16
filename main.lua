7bdefed6a240444e2bf5345a4e37c4e5abe6c8af
Daniel Mattos <39279358+summonsumo@users.noreply.github.com>
2023-08-16 16:38:12 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -6,7 +6,6 @@ physics.start()
 physics.pause()
 physics.setGravity(0, 0)
 
-physics.setDrawMode( "hybrid" )
 
 display.setStatusBar( display.HiddenStatusBar )
 native.setProperty( "windowMode", "maximized" )
@@ -15,7 +14,7 @@ audio.reserveChannels( 1 )
 audio.setMaxVolume(0.6,{channel = 1})
 audio.setMaxVolume(0.3,{channel = 2})
     
-composer.gotoScene("Levels.firstlevel",{effect = "fade", time = 1000}) 
+composer.gotoScene("Levels.intro",{effect = "fade", time = 1000}) 
 --Change to Levels.intro
 
 
